dda64bbaf6a8c8c8190c550c20972ed6a87540b4
Zhanchao Yang <zhanchao@upenn.edu>
2025-11-04 17:23:39 -0500

    finalize

--- scripts/2017_ent__od_city_suburb_link.R
+++ scripts/2017_ent__od_city_suburb_link.R
@@ -23,7 +23,7 @@ suburb_to_city <- trip_2017 %>%
   filter(origin_ent %in% c("13", "15") & dest_ent == "9") %>%
   select(starts_with("P5_14"), FACTOR)
 complete_suburb_city <- rbind(city_to_suburb, suburb_to_city)
-
+sum(complete_suburb_city$FACTOR)
 #turns all 2 to NA
 complete_suburb_city[complete_suburb_city == 2] <- NA
 
